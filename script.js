792593308b0f0e23b55bacdb173aa47cdb0bed98
al-khalaf <osamalaith2006@gmail.com>
2022-08-18 00:22:58 +0300

    Classwork7

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+function BMI(weight, height){
+    result = weight/ (height*height)
+    return result
+}
+
+console.log(BMI(95,175))
+
+function Status(bmi){
+    if (bmi<18.5){
+        return("Below Weight")
+    }
+    else if (bmi>=18.5 && bmi<25){
+        return("Perfect Weight!!")
+    }
+    else if (bmi>=25){
+        return("OverWeighted")
+    }
+}
+
+function calculate(){
+    let weight = document.getElementById("weight").value
+    let height = document.getElementById("height").value
+    let bmi = BMI(weight, height)
+    let desc = Status(bmi)
+    let div = document.getElementById('result')
+    div.innerTEXT = bmi + " == " + desc
+}   
 
